73b65302c89882dc3b311ddbc478812152976495
Darshan Karthik A M <darshankarthikam@gmail.com>
2021-08-25 09:45:36 +0530

    Adding new Object

--- BankingApplication/src/com/training/org/XYZBank.java
+++ BankingApplication/src/com/training/org/XYZBank.java
@@ -4,5 +4,7 @@ public class XYZBank {
     public static void main(String[] args) {
         XYZ obj = new XYZ();
         System.out.println(obj);
+        XYZ obj1 = new XYZ("canara","CRP",200);
+        System.out.println(obj1);
     }
 }
